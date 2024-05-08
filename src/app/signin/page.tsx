567e31119edc20d559b5cf27667cafbcfa0abff2
Vivek Joshi <vrj022@gmail.com>
2024-05-08 19:20:39 +0530

    Sign in Form, Validation checks added.

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,20 +1,81 @@
 'use client';
 import {useAppDispatch} from "@/store";
 import {setAuthState} from "@/store/authSlice";
-import {useEffect} from "react";
+import {useEffect, useRef, useState} from "react";
+import Username from "@/app/signin/username";
+import Password from "@/app/signin/password";
+import {useRouter} from "next/navigation";
 
 const Page = () => {
+    const router = useRouter();
     const dispatch = useAppDispatch();
+
+    const [user, setUser] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [usernameError, setUsernameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [signInEnabled, setSigninEnabled] = useState(false);
+    const mountCount = useRef(0);
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+
+    }
+
     useEffect(() => {
 
     }, []);
+
+    useEffect(() => {
+        if (mountCount.current > 0 && !usernameError && !passwordError) {//Sign in button should remain disabled on initial mount.
+            setSigninEnabled(true);
+        } else {
+            setSigninEnabled(false);
+        }
+        mountCount.current += 1;
+    }, [usernameError, passwordError]);
     return (
         <>
-            <div className=''>
-                <h2>This is Sign in Page</h2>
-                <button onClick={() => dispatch(setAuthState(true))}>Log in</button><br />
-                <button onClick={() => dispatch(setAuthState(false))}>Log out</button>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        className="mx-auto h-10 w-auto"
+                        src="/amzlogo2.svg"
+                        alt="Your Company"
+                    />
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Sign in to your account
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
+                        <Username setUser={setUser} usernameError={usernameError} setUsernameError={setUsernameError} />
+                        <Password setPassword={setPassword} passwordError={passwordError} setPasswordError={setPasswordError} />
+                        <div>
+                            <button
+                                type="submit"
+                                className={`flex w-full justify-center rounded-md bg-yellow-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${!signInEnabled && 'opacity-50'}`}
+                             disabled={!signInEnabled}>
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Not a member?{' '}
+                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500" onClick={() => router.push('/signup')}>
+                            Start here
+                        </a>
+                    </p>
+                </div>
             </div>
+
+            {/*<div className=''>*/}
+            {/*    <h2>This is Sign in Page</h2>*/}
+            {/*    <button onClick={() => dispatch(setAuthState(true))}>Log in</button><br />*/}
+            {/*    <button onClick={() => dispatch(setAuthState(false))}>Log out</button>*/}
+            {/*</div>*/}
         </>
     )
 }
---
+++ src/app/signin/password.tsx
@@ -0,0 +1,41 @@
+const Password = ({ setPassword, passwordError, setPasswordError }) => {
+    const handlePassword = (e: any) => {
+        if (e.target.value != '') {
+            setPassword(e.target.value);
+            setPasswordError(false);
+        } else {
+            setPasswordError(true);
+        }
+    }
+
+    return (
+        <>
+            <div>
+                <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                        Password
+                    </label>
+                    <div className="text-sm">
+                        <a href="#" className="font-semibold text-black hover:text-black">
+                            Forgot password?
+                        </a>
+                    </div>
+                </div>
+                <div className="mt-2">
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        onBlur={handlePassword}
+                    />
+                    <label className={`text-sm text-red-500 ${!passwordError && 'hidden'}`}>Please enter valid Password</label>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Password;
