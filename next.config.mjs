99ef77f34cfa6a3c6e0df635538895d98dcd9547
Vivek Joshi <vrj022@gmail.com>
2024-05-18 21:55:19 +0530

    Horizontal Scroll for Popular products.

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
     },
     images: {
         domains: ['e-commerce-nestjs-images.s3.amazonaws.com'],
+        deviceSizes: [640, 750, 1080, 1200, 1920],
+        imageSizes: [16, 32, 48, 64, 96],
     },
 };
 
--- src/app/dashboard/byBrand.tsx
+++ src/app/dashboard/byBrand.tsx
@@ -27,32 +27,38 @@ const ByBrand = () => {
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-7xl lg:px-8">
                 <h2 className="mb-6 font-bold text-2xl">Popular Products by Brand</h2>
 
-                <div
-                    className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 xl:gap-x-8">
-                    {products.map((product: any) => (
-                        <a key={product.product_id} href='#'
-                           onClick={() => router.push(`/product/${product.product_id}`)} className="group">
-                            <div
-                                className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-800 xl:aspect-h-8 xl:aspect-w-7">
-                                {
-                                    product.image &&
-                                    <Image
-                                        src={product.image[0]}
-                                        alt=''
-                                        className="p-1 h-full w-full object-cover object-center group-hover:opacity-75"
-                                        width={0}
-                                        height={0}
-                                        layout='responsive'
-                                    />
-                                }
-                            </div>
-                            <h3 className="mt-4 font-bold text-sm text-gray-700">{product.brand}</h3>
-                            <h3 className="text-sm text-gray-700">{product.product_name}</h3>
-                            <p className="mt-1 text-lg font-medium text-gray-900">
-                                <span className='text-sm'>&#8377;</span>{product.price}
-                            </p>
-                        </a>
-                    ))}
+                <div className="w-full">
+                    <div className="overflow-x-auto no-scrollbar">
+                        <div className="flex space-x-8">
+                            {products.map((product: any) => (
+                                <a
+                                    key={product.product_id}
+                                    href="#"
+                                    onClick={() => router.push(`/product/${product.product_id}`)}
+                                    className="min-w-[18%] group"
+                                >
+                                    <div
+                                        className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-800 xl:aspect-h-8 xl:aspect-w-7">
+                                        {product.image && (
+                                            <Image
+                                                src={product.image[0]}
+                                                alt=""
+                                                className="p-1 h-full w-full object-cover object-center group-hover:opacity-75"
+                                                width={0}
+                                                height={0}
+                                                layout="responsive"
+                                            />
+                                        )}
+                                    </div>
+                                    <h3 className="mt-4 font-bold text-sm text-gray-700">{product.brand}</h3>
+                                    <h3 className="text-sm text-gray-700">{product.product_name}</h3>
+                                    <p className="mt-1 text-lg font-medium text-gray-900">
+                                        <span className="text-sm">&#8377;</span>{product.price}
+                                    </p>
+                                </a>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
